6f5103764407e909fc13adf869f9256de59d3c49
Matheus Damião <matheus.damiaoliveira@gmail.com>
2024-07-30 14:24:32 -0300

    update hero section copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,7 @@ export default function Home() {
       <section className="w-full h-[80svh] lg:h-[90svh] relative animatedBg flex flex-col items-start justify-center">
         <div className="max-w-7xl w-full m-auto flex items-center flex-col gap-14 px-6 lg:px-0">
           <h1 className="text-xl lg:text-[28px] leading-loose lg:m-auto tracking-[0.2rem] relative text-[#e2e1e1]    text-center lg:w-[70%]">
-            Fomentamos o sucesso de empreendimentos de grande potencial
-            para transformar o presente em um futuro de grandes possibilidades
+          Fomentamos o sucesso de negócios e empreendedores de grande potencial<span className="font-body text-4xl font-bold">,</span> focados em transformar o presente em um futuro de grandes possibilidades
           </h1>
           {/* <h2 className="lg:text-xl font-body font-light text-center lg:w-[60%] m-auto text-[#BCC1D1] ">
             Nosso compromisso é potencializar empresas que resolvam problemas e
